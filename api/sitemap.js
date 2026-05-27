6e936a7dc0c80f8135e1eb14e2f154bcc3985cb4
Andrey-Dude <105594042+Andrey-Dude@users.noreply.github.com>
2026-05-27 11:25:49 +0500

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -4,7 +4,7 @@ export const config = {
 
 export default async function handler(req) {
   const response = await fetch(
-    'https://www.lookdigitalsignage.com/sitemap.xml'
+    'https://look-new.webflow.io/sitemap.xml'
   );
   const xml = await response.text();
 
